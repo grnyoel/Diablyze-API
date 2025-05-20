2467a18127ea691a48108fa85d250b23fab6e242
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2025-05-20 22:12:28 +0800

    register-login controller

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const authService = require('../services/authService');
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+const authController = {
+  register: async (req, res, next) => {
+    try {
+      const result = await authService.register(req.body);
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const result = await authService.login(req.body);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+}
+
+module.exports = authController;
