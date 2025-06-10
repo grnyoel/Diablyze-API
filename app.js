b0de6853b0574339b42a823f46c1489b6052e946
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2025-06-11 03:11:36 +0800

    handle cors

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const { errorHandlerMiddleware } = require('./middlewares/errorMiddleware');
+const DiabetesModel = require('./ml-models/DiabetesModel');
 
-const PORT = process.env.PORT;
+// Load Model
+// (async () => {
+//   try {
+//     await DiabetesModel.loadModel();
+//     console.log('✅ ML Model ready');
+//   } catch (error) {
+//     console.error('❌ Failed to load ML model:', error);
+//   }
+// })();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/', authRoutes);
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'API is healthy' });
+});
+
+// Error handling middleware
+app.use(errorHandlerMiddleware);
+
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
