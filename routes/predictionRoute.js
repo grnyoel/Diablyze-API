6a4f829ea41f4c7e8f46db66c10dc6f96def1bfb
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2025-06-12 07:38:59 +0800

    add auth require before predict

--- routes/predictionRoute.js
+++ routes/predictionRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 const { predictDiabetes } = require('../controllers/predictionController');
-const { requireAuth } = require('../middlewares/authMiddleware');
 const { validatePrediction } = require('../middlewares/validateRequest');
 
-router.post('/predict', 
-  requireAuth,
+router.post('/predict',
+  authMiddleware,
   validatePrediction,
   predictDiabetes
 );
